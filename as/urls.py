f7603283847262508a703ad563fd889e6b42016b
xxy <43472994+xxy12983@users.noreply.github.com>
2020-12-12 21:00:34 +0800

    Update urls.py
    search界面url添加

--- as/urls.py
+++ as/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('user_system/', include('user_system.urls')),
     path('admin/', admin.site.urls),
+    path('data/search/', include('search.urls')),
 ]
