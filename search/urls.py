825ed3e25e572ca22d0a22cf56e104317c391d8c
xxy <43472994+xxy12983@users.noreply.github.com>
2020-12-23 16:26:57 +0800

    add urls

--- search/urls.py
+++ search/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
     path('papers/', views.search_papers),
     path('discussion/',views.search_discussions)
+    path('new_papers/', views.search_new_papers),
+    path('new_discussion/', views.search_new_discussions),
+    path('hot_discussion/', views.search_hot_discussions),
 ]
