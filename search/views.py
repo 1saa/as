ff925062957436b2d62fef99da1fb30a33f7a768
xxy <43472994+xxy12983@users.noreply.github.com>
2020-12-27 01:09:58 +0800

    Update views.py

--- search/views.py
+++ search/views.py
@@ -66,273 +66,238 @@ def cors_Jsresponse(ret):
 @require_cors_GET
 #@check_login
 def search_results(request):
-    if (request.method == 'OPTIONS'):
-        response_options()
-    if request.method != 'GET':
-        return cors_Jsresponse({'ret':1,'msg':'不支持此类型的http请求'})
-    else:
-        paperset = Papers.objects.values()
-        Type = request.GET.get('Type', None)
-        if(Type):
-            if(Type == 'paper'):
-                return search_papers(request)
-            elif(Type == 'discussion'):
-                return search_discussions(request)
-            else:
-                return cors_Jsresponse({'ret': 1, 'msg': 'Type内容非法'})
+    Type = request.GET.get('Type', None)
+    if(Type):
+        if(Type == 'paper'):
+            return search_papers(request)
+        elif(Type == 'discussion'):
+            return search_discussions(request)
         else:
-            return getall(request)
+            return cors_Jsresponse({'ret': 1, 'msg': 'Type内容非法'})
+    else:
+        return getall(request)
 
 @csrf_exempt
 @require_cors_GET
 #@check_login
 def getall(request):
-    if (request.method == 'OPTIONS'):
-        response_options()
-    if request.method != 'GET':
-        return JsonResponse({'ret': 1, 'msg': '不支持此类型的http请求'})
-    else:
-        paperset = Papers.objects.values()
-        discussionset = Discussion.objects.values()
-        na = request.GET.get('Keywords', None)
-        na2 = request.GET.get('KeywordsEntire', None)
-        na3 = request.GET.get('KeywordsAvoid', None)
-        na4 = request.GET.get('KeywordsAvoidEntire', None)
-        au = request.GET.get('Authors', None)
-        ta = request.GET.get('Tags', None)
-        if na:
-            paperset = paperset.filter(name=na)
-            discussionset = discussionset.filter(name=na)
-        if na2:
-            paperset = paperset.filter(name=na)
-            discussionset = discussionset.filter(name=na)
-        if na3:
-            paperset = paperset.filter(name=na)
-            discussionset = discussionset.filter(name=na)
-        if na4:
-            paperset = paperset.filter(name=na)
-            discussionset = discussionset.filter(name=na)
-        if au:
-            paperset = paperset.filter(publisher__name=au)
-            discussionset = discussionset.filter(creator=au)
-        count = 0
-        to_be_deleted = []
-        to_be_deleted2 = []
-        if ta:
-            for paper in paperset:
-                if ta not in paper["tag_list"]:
-                    to_be_deleted.append(paper["id"])
-            for dis in discussionset:
-                if ta not in dis["tag_list"]:
-                    to_be_deleted2.append(dis["id"])
-        paperset = paperset.filter(id__in = to_be_deleted).delete()
-        discussionset = discussionset.filter(id__in=to_be_deleted2).delete()
-        retlist = []
-        if(paperset.exists()):
-            paperset = paperset.order_by('-create_time')
-            for paper in paperset:
-                count = count + 1
-                retlist = retlist.qppend(
-                    {
+    paperset = Papers.objects.values()
+    discussionset = Discussion.objects.values()
+
+    na = request.GET.get('Keywords', None)
+    na2 = request.GET.get('KeywordsEntire', None)
+    na3 = request.GET.get('KeywordsAvoid', None)
+    na4 = request.GET.get('KeywordsAvoidEntire', None)
+    au = request.GET.get('Authors', None)
+    ta = request.GET.get('Tags', None)
+    if na:
+        paperset = paperset.filter(name=na)
+        discussionset = discussionset.filter(name=na)
+    if na2:
+        paperset = paperset.filter(name=na)
+        discussionset = discussionset.filter(name=na)
+    if na3:
+        paperset = paperset.filter(name=na)
+        discussionset = discussionset.filter(name=na)
+    if na4:
+        paperset = paperset.filter(name=na)
+        discussionset = discussionset.filter(name=na)
+    if au:
+        paperset = paperset.filter(publisher__name=au)
+        discussionset = discussionset.filter(creator=au)
+    count = 0
+    to_be_deleted = []
+    to_be_deleted2 = []
+    if ta:
+        for paper in paperset:
+            if ta not in paper["tag_list"]:
+                to_be_deleted.append(paper["id"])
+        for dis in discussionset:
+            if ta not in dis["tag_list"]:
+                to_be_deleted2.append(dis["id"])
+    paperset = paperset.exclude(id__in=to_be_deleted)
+    discussionset = discussionset.exclude(id__in=to_be_deleted2)
+    retlist = []
+    if paperset.exists():
+        paperset = paperset.order_by('-create_time')
+        for paper in paperset:
+            count = count + 1
+            retlist = retlist.append(
+                {
+                'key': count,
+                'id': paper["id"],
+                'type': 'paper',
+                'title': paper["name"],
+                'abstract':paper["infomation"],
+                'tags': paper["tag_list"],
+                'authors': paper["publisher"],
+                'publishtime':paper["pubyear"],
+                'updatetime':paper["last_time"],
+                }
+            )
+    if discussionset.exists():
+        discussionset = discussionset.order_by('-create_time')
+        for dis_2 in discussionset:
+            count = count + 1
+            retlist = retlist.append(
+                {
                     'key': count,
-                    'id': paper["id"],
-                    'type': 'paper',
-                    'title': paper["name"],
-                    'abstract':paper["infomation"],
-                    'tags': paper["tag_list"],
-                    'authors': paper["publisher"],
-                    'publishtime':paper["pubyear"],
-                    'updatetime':paper["last_time"],
-                    }
-                )
-        if (discussionset.exists()):
-            discussionset = discussionset.order_by('-create_time')
-            for dis_2 in discussionset:
-                count = count + 1
-                retlist = retlist.qppend(
-                    {
-                        'key': count,
-                        'id': dis_2["id"],
-                        'type': 'discussion',
-                        'title': dis_2["title"],
-                        'abstract': '',
-                        'tags': dis_2["tag_list"],
-                        'authors': dis_2["creator"],
-                        'publishtime': dis_2["creat_time"],
-                        'updatetime': dis_2["last_time"],
-                    }
-                )
-        response = JsonResponse(retlist,safe=False)
-        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-        return response
+                    'id': dis_2["id"],
+                    'type': 'discussion',
+                    'title': dis_2["title"],
+                    'abstract': '',
+                    'tags': dis_2["tag_list"],
+                    'authors': dis_2["creator"],
+                    'publishtime': dis_2["creat_time"],
+                    'updatetime': dis_2["last_time"],
+                }
+            )
+    response = JsonResponse(retlist,safe=False)
+    response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return response
 
 @csrf_exempt
 @require_cors_GET
 #@check_login
 def search_papers(request):
-    if (request.method == 'OPTIONS'):
-        response_options()
-    if request.method != 'GET':
-        return JsonResponse({'ret': 1, 'msg': '不支持此类型的http请求'})
-    else:
-        paperset = Papers.objects.values()
-        na = request.GET.get('Keywords', None)
-        na2 = request.GET.get('KeywordsEntire', None)
-        na3 = request.GET.get('KeywordsAvoid', None)
-        na4 = request.GET.get('KeywordsAvoidEntire', None)
-        au = request.GET.get('Authors', None)
-        ta = request.GET.get('Tags', None)
-        if na:
-            paperset = paperset.filter(name=na)
-        if na2:
-            paperset = paperset.filter(name=na)
-        if na3:
-            paperset = paperset.filter(name=na)
-        if na4:
-            paperset = paperset.filter(name=na)
-        if au:
-            paperset = paperset.filter(publisher__name=au)
-        count = 0
-        to_be_deleted = []
-        if ta:
-            for paper in paperset:
-                if ta not in paper["tag_list"]:
-                    to_be_deleted.append(paper["id"])
-        paperset = paperset.filter(id__in = to_be_deleted).delete()
-        retlist = []
-        if(paperset.exists()):
-            paperset = paperset.order_by('-create_time')
-            for paper in paperset:
-                count = count + 1
-                retlist = retlist.qppend(
-                    {
-                        'key': count,
-                        'id': paper["id"],
-                        'type': 'paper',
-                        'title': paper["name"],
-                        'abstract': paper["infomation"],
-                        'tags': paper["tag_list"],
-                        'authors': paper["publisher"],
-                        'publishtime': paper["pubyear"],
-                        'updatetime': paper["last_time"],
-                    }
-                )
-        response = JsonResponse(retlist,safe=False)
-        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-        return response
+    paperset = Papers.objects.values()
+    na = request.GET.get('Keywords', None)
+    na2 = request.GET.get('KeywordsEntire', None)
+    na3 = request.GET.get('KeywordsAvoid', None)
+    na4 = request.GET.get('KeywordsAvoidEntire', None)
+    au = request.GET.get('Authors', None)
+    ta = request.GET.get('Tags', None)
+    if na:
+        paperset = paperset.filter(name=na)
+    if na2:
+        paperset = paperset.filter(name=na)
+    if na3:
+        paperset = paperset.filter(name=na)
+    if na4:
+        paperset = paperset.filter(name=na)
+    if au:
+        paperset = paperset.filter(publisher__name=au)
+    count = 0
+    to_be_deleted = []
+    if ta:
+        for paper in paperset:
+            if ta not in paper["tag_list"]:
+                to_be_deleted.append(paper["id"])
+    paperset = paperset.exclude(id__in = to_be_deleted)
+    retlist = []
+    if  paperset.exists():
+        paperset = paperset.order_by('-create_time')
+        for paper in paperset:
+            count = count + 1
+            retlist = retlist.append(
+                {
+                    'key': count,
+                    'id': paper["id"],
+                    'type': 'paper',
+                    'title': paper["name"],
+                    'abstract': paper["infomation"],
+                    'tags': paper["tag_list"],
+                    'authors': paper["publisher"],
+                    'publishtime': paper["pubyear"],
+                    'updatetime': paper["last_time"],
+                }
+            )
+    response = JsonResponse(retlist,safe=False)
+    response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return response
 @csrf_exempt
 @require_cors_GET
 @check_login
 def search_new_papers(request):
-    if (request.method == 'OPTIONS'):
-        response_options()
-    if request.method != 'GET':
-        return JsonResponse({'ret':1,'msg':'不支持此类型的http请求'})
+    paperset = Papers.objects.values()
+    if paperset.exists():
+        paperset = paperset.order_by('-create_time')
+        retlist = list(paperset)
+        response = JsonResponse({'ret': 0,'retlist': retlist})
+        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        return response
     else:
-        paperset = Papers.objects.values()
-        if paperset.exists():
-            paperset = paperset.order_by('-create_time')
-            retlist = list(paperset)
-            response = JsonResponse({'ret': 0,'retlist': retlist})
-            response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-            return response
-        else:
-            response = JsonResponse({'ret': 1, 'msg': '论文库为空'})
-            response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-            return response
+        response = JsonResponse({'ret': 1, 'msg': '论文库为空'})
+        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        return response
 
 @csrf_exempt
 @require_cors_GET
 #@check_login
 def search_discussions(request):
-    if (request.method == 'OPTIONS'):
-        response_options()
-    if request.method != 'GET':
-        return JsonResponse({'ret': 1, 'msg': '不支持此类型的http请求'})
-    else:
-        discussionset = Discussion.objects.values()
-        na = request.GET.get('Keywords', None)
-        na2 = request.GET.get('KeywordsEntire', None)
-        na3 = request.GET.get('KeywordsAvoid', None)
-        na4 = request.GET.get('KeywordsAvoidEntire', None)
-        au = request.GET.get('Authors', None)
-        ta = request.GET.get('Tags', None)
-        if na:
-            discussionset = discussionset.filter(name=na)
-        if na2:
-            discussionset = discussionset.filter(name=na)
-        if na3:
-            discussionset = discussionset.filter(name=na)
-        if na4:
-            discussionset = discussionset.filter(name=na)
-        if au:
-            discussionset = discussionset.filter(creator=au)
-        count = 0
-        to_be_deleted = []
-        retlist = []
-        if ta:
-            for dis in discussionset:
-                if ta not in dis["tag_list"]:
-                    to_be_deleted.append(dis["id"])
-        discussionset = discussionset.filter(id__in=to_be_deleted).delete()
-        if (discussionset.exists()):
-            discussionset = discussionset.order_by('-create_time')
-            for dis_2 in discussionset:
-                count = count + 1
-                retlist = retlist.qppend(
-                    {
-                        'key': count,
-                        'id': dis_2["id"],
-                        'type': 'discussion',
-                        'title': dis_2["title"],
-                        'abstract': '',
-                        'tags': dis_2["tag_list"],
-                        'authors': dis_2["creator"],
-                        'publishtime': dis_2["creat_time"],
-                        'updatetime': dis_2["last_time"],
-                    }
-                )
-        response = JsonResponse(retlist, safe=False)
-        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-        return response
+    discussionset = Discussion.objects.values()
+    na = request.GET.get('Keywords', None)
+    na2 = request.GET.get('KeywordsEntire', None)
+    na3 = request.GET.get('KeywordsAvoid', None)
+    na4 = request.GET.get('KeywordsAvoidEntire', None)
+    au = request.GET.get('Authors', None)
+    ta = request.GET.get('Tags', None)
+    if na:
+        discussionset = discussionset.filter(name=na)
+    if na2:
+        discussionset = discussionset.filter(name=na)
+    if na3:
+        discussionset = discussionset.filter(name=na)
+    if na4:
+        discussionset = discussionset.filter(name=na)
+    if au:
+        discussionset = discussionset.filter(creator=au)
+    count = 0
+    to_be_deleted = []
+    retlist = []
+    if ta:
+        for dis in discussionset:
+            if ta not in dis["tag_list"]:
+                to_be_deleted.append(dis["id"])
+    discussionset = discussionset.exclude(id__in=to_be_deleted)
+    if discussionset.exists():
+        discussionset = discussionset.order_by('-create_time')
+        for dis_2 in discussionset:
+            count = count + 1
+            retlist = retlist.append(
+                {
+                    'key': count,
+                    'id': dis_2["id"],
+                    'type': 'discussion',
+                    'title': dis_2["title"],
+                    'abstract': '',
+                    'tags': dis_2["tag_list"],
+                    'authors': dis_2["creator"],
+                    'publishtime': dis_2["creat_time"],
+                    'updatetime': dis_2["last_time"],
+                }
+            )
+    response = JsonResponse(retlist, safe=False)
+    response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    return response
 @csrf_exempt
 @require_cors_GET
 @check_login
 def search_hot_discussions(request):
-    if (request.method == 'OPTIONS'):
-        response_options()
-    if request.method != 'GET':
-        return JsonResponse({'ret':1,'msg':'不支持此类型的http请求'})
+    discussionset = Discussion.objects.values()
+    if discussionset.exists():
+        discussionset = discussionset.order_by('-reply_number')
+        retlist = list(discussionset)
+        response =  JsonResponse({'ret': 0, 'retlist': retlist})
+        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        return response
     else:
-        discussionset = Discussion.objects.values()
-        if discussionset.exists():
-            discussionset = discussionset.order_by('-reply_number')
-            retlist = list(discussionset)
-            response =  JsonResponse({'ret': 0, 'retlist': retlist})
-            response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-            return response
-        else:
-            response =  JsonResponse({'ret': 1, 'msg': '找不到此类型讨论'})
-            response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-            return response
+        response =  JsonResponse({'ret': 1, 'msg': '找不到此类型讨论'})
+        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        return response
 
 @csrf_exempt
 @require_cors_GET
 @check_login
 def search_new_discussions(request):
-    if (request.method == 'OPTIONS'):
-        response_options()
-    if request.method != 'GET':
-        return JsonResponse({'ret': 1, 'msg': '不支持此类型的http请求'})
+    discussionset = Discussion.objects.values()
+    if discussionset.exists():
+        discussionset = discussionset.order_by('-last_time')
+        retlist = list(discussionset)
+        response = JsonResponse({'ret': 0, 'retlist': retlist})
+        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        return response
     else:
-        discussionset = Discussion.objects.values()
-        if discussionset.exists():
-            discussionset = discussionset.order_by('-last_time')
-            retlist = list(discussionset)
-            response = JsonResponse({'ret': 0, 'retlist': retlist})
-            response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-            return response
-        else:
-            response = JsonResponse({'ret': 1, 'msg': '找不到此类型讨论'})
-            response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-            return response
+        response = JsonResponse({'ret': 1, 'msg': '找不到此类型讨论'})
+        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        return response
